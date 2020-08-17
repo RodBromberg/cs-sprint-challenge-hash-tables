31fee35ccf761554db8ce7d55c502178b179193e
Rod <rod.bromberg@yahoo.com>
2020-08-16 20:19:27 -0400

    ex 4 complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,25 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    map = {}
+
+    result = []
+
+    for i in range(len(a)):
+        if(map.get(a[i]) == None):
+            map[a[i]] = 1
+        else:
+            map[a[i]] = map[a[i]]+1
+
+    for num in a:
+        if(map.get(num*-1) == map.get(num) and num > 0):
+            result.append(num)
 
     return result
 
 
-if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+# print('gg')
+print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+
+# if __name__ == "__main__":
+#     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
